1c7b1b76c3871708bb0dbb9d3610162e05802419
Your Name <youremail@example.com>
2023-07-31 20:29:14 -0500

    fix login

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <script setup>
-import { onBeforeMount } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
+import { ref, onMounted } from 'vue'
 
-onBeforeMount(async () => {
+const isLoggedIn = ref(false)
+
+onMounted(async () => {
   // call the /.auth/me endpoint to see if we're logged in
   // if not, redirect to the login page
   const me = await getMe()
 
-  console.log(me)
-
-  if (!me) {
-    window.location.href = '/login.html'
+  if (me) {
+    isLoggedIn.value = true
   }
 })
 
@@ -23,7 +22,7 @@ async function getMe() {
 </script>
 
 <template>
-  <main class="container is-flex is-flex-direction-column">
+  <main class="container is-flex is-flex-direction-column" v-if="isLoggedIn">
     <nav class="pt-5 mb-5">
       <div class="columns is-vcentered">
         <div class="column">
@@ -36,11 +35,29 @@ async function getMe() {
     </nav>
     <RouterView />
   </main>
+  <div v-else>
+    <div class="container">
+      <div class="centered">
+        <div style="text-align: center">
+          <h1 class="title">Advocacy AI Assistant</h1>
+          <h2 class="subtitle">Log in with your Microsoft email address</h2>
+          <a class="button is-success" href="/.auth/login/aad">Log In</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .container {
   height: 100%;
   min-height: 100%;
+}
+
+.centered {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
 </style>
