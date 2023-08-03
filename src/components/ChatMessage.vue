2d0dfb1d51e231c59b62a43de4d09c3a68a35f3f
Your Name <youremail@example.com>
2023-08-03 17:35:48 -0500

    I'm not a fan of this spacing but here goes

--- src/components/ChatMessage.vue
+++ src/components/ChatMessage.vue
@@ -25,29 +25,27 @@ defineProps({
 </script>
 
 <template>
-  <div class="columns">
-    <div class="column">
-      <div class="has-text-right">
-        <button
-          class="delete is-small"
-          @click="deleteMessage(message.id)"
-          v-if="message.role === 'user'"
-        ></button>
-        <button
-          class="button is-small"
-          @click="copyMessage(message.contentPlain)"
-          v-if="message.role === 'assistant'"
-          title="Copy to clipbard"
-        >
-          <span v-if="showCopyNotification" class="mr-3">Copied!</span>
-          <span v-else>Copy</span>
-          <span class="icon is-small">
-            <i class="fas fa-clipboard"></i>
-          </span>
-        </button>
-      </div>
-      <div class="content message-content" v-html="message.content" ref="message"></div>
+  <div>
+    <div class="has-text-right mb-2">
+      <button
+        class="delete is-small"
+        @click="deleteMessage(message.id)"
+        v-if="message.role === 'user'"
+      ></button>
+      <button
+        class="button is-small"
+        @click="copyMessage(message.contentPlain)"
+        v-if="message.role === 'assistant'"
+        title="Copy to clipbard"
+      >
+        <span v-if="showCopyNotification" class="mr-3">Copied!</span>
+        <span v-else>Copy</span>
+        <span class="icon is-small">
+          <i class="fas fa-clipboard"></i>
+        </span>
+      </button>
     </div>
+    <div class="content message-content" v-html="message.content" ref="message"></div>
   </div>
 </template>
 
