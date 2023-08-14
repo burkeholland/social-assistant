8e310d045ed3ef336dad467c1cc58b2c07a66e4a
Your Name <youremail@example.com>
2023-08-14 21:10:20 +0000

    Add devcontainer

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install the Azure Static Web Apps CLI
+npm install -g @azure/static-web-apps-cli@1.1.3
+
+# Install the Azure Functions Core Tools
+npm install -g azure-functions-core-tools@4 --unsafe-perm true
