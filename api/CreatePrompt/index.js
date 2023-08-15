55324d68bfc84dbe8bda8a12a0c316e5a5fb9906
Burke Holland <burkeholland@gmail.com>
2023-08-15 17:22:23 +0000

    add some err handling on create prompt

--- api/CreatePrompt/index.js
+++ api/CreatePrompt/index.js
@@ -2,33 +2,40 @@ const promptsService = require('../services/promptsService')
 const authService = require('../services/authService')
 
 module.exports = async function (context, req) {
-  const clientPrincipal = authService.isUserAuthorized(req, context)
-  if (clientPrincipal === null) {
-    return unauthorizedResponse(context)
-  }
-
-  const { title, category, text } = req.body
-
-  const promptItem = await promptsService.getPromptByTitle(title)
-
-  if (promptItem.length > 0) {
-    context.res = {
-      status: 409,
-      body: { body: 'Prompt already exists' }
+  try {
+    const clientPrincipal = authService.isUserAuthorized(req)
+    if (clientPrincipal === null) {
+      return unauthorizedResponse(context)
     }
-    return
-  }
 
-  const createdItem = await promptsService.createPrompt(
-    title,
-    category,
-    text,
-    clientPrincipal.userId
-  )
+    const { title, category, text } = req.body
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: createdItem
+    const promptItem = await promptsService.getPromptByTitle(title)
+
+    if (promptItem.length > 0) {
+      context.res = {
+        status: 409,
+        body: { body: 'Prompt already exists' }
+      }
+      return
+    }
+
+    const createdItem = await promptsService.createPrompt(
+      title,
+      category,
+      text,
+      clientPrincipal.userId
+    )
+
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: createdItem
+    }
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: error.message
+    }
   }
 }
 
