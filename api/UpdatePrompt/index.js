480457b0447bd7d30fa2df6d9c4077a4154fb445
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2023-08-26 16:04:36 +0000

    make api functional

--- api/UpdatePrompt/index.js
+++ api/UpdatePrompt/index.js
@@ -2,32 +2,48 @@ const authService = require('../services/authService')
 const promptsService = require('../services/promptsService')
 
 module.exports = async function (context, req) {
-  const clientPrincipal = authService.isUserAuthorized(req, context)
-  if (clientPrincipal === null) {
-    return unauthorizedResponse(context)
-  }
+  try {
+    const clientPrincipal = authService.isUserAuthorized(req, context)
+    if (clientPrincipal === null) {
+      return unauthorizedResponse(context)
+    }
 
-  const { id } = req.params
-  const { title, category, text } = req.body
+    const { id } = req.params
+    const { title, category, text } = req.body
 
-  // first, see if the item exists in the database already
-  const existingPrompt = await promptsService.getPrompt(id)
 
-  // the prompt already exists, so we need to check to see if the user has the right to update it
-  if (existingPrompt === null || existingPrompt.userId !== clientPrincipal.userId) {
-    return unauthorizedResponse(context)
-  }
+    // first, see if the item exists in the database already
+    const existingPrompt = await promptsService.getPrompt(id)
 
-  const resource = await promptsService.savePrompt(
-    id,
-    title,
-    category,
-    text,
-    clientPrincipal.userId
-  )
+    console.log({ existingPrompt })
+    // the prompt already exists, so we need to check to see if the user has the right to update it
+    if (!existingPrompt || existingPrompt.userId !== clientPrincipal.userId) {
+      return unauthorizedResponse(context)
+    }
 
-  context.res = {
+    const resource = await promptsService.updatePrompt(
+      id,
+      title,
+      category,
+      text,
+      clientPrincipal.userId
+    )
+
+    context.res = {
     // status: 200, /* Defaults to 200 */
-    body: responseMessage
+      body: resource
+    }
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: error.message
+    }
+  }
+}
+
+function unauthorizedResponse(context) {
+  context.res = {
+    status: 401,
+    body: 'Unauthorized'
   }
 }
