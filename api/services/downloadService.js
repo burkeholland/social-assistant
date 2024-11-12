5fc0864f9005ea471db99eb816ec6476fc09c13d
burkeholland <burkeholland@gmail.com>
2024-11-12 16:58:39 +0000

    fix: update downloadService to return full videoInfo instead of just title

--- api/services/downloadService.js
+++ api/services/downloadService.js
@@ -51,7 +51,7 @@ const downloadService = {
 
       return {
         status: 200,
-        body: { type: 'captions', content: videoInfo.title }
+        body: JSON.stringify(videoInfo)
       }
     } catch (error) {
       return { status: 500, body: error.message }
